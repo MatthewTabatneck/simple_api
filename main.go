6e67b66e5eb1b1d909f25415eec93e41db759a90
MatthewTabatneck <mmtabat3@gmail.com>
2025-09-09 20:31:46 -0400

    set up to store locally in 'users'

--- main.go
+++ main.go
@@ -1,1 +1,27 @@
 package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func uhoh(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func usersHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+var users []string
+
+func main() {
+
+	http.HandleFunc("/users", usersHandler)
+
+	err := http.ListenAndServe(":8080", nil)
+	uhoh(err)
+
+}
