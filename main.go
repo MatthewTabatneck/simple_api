caa35d2032e85733fc3a1955d84eb68323cf987e
MatthewTabatneck <mmtabat3@gmail.com>
2025-09-10 21:12:15 -0400

    Testing connecting to db

--- main.go
+++ main.go
@@ -1,51 +1,73 @@
 package main
 
 import (
-	"encoding/json"
-	"net/http"
+	"database/sql"
+	// "encoding/json"
+	"fmt"
+	// "net/http"
+
+	_ "github.com/lib/pq"
 )
 
-func usersHandler(w http.ResponseWriter, r *http.Request) {
-	//switch case that acats baased on either GET or POST
-	switch r.Method {
+const (
+	host     = "localhost"
+	port     = 5431
+	user     = "postgres"
+	password = "mysecretpassword"
+	dbname   = "postgres"
+)
 
-	case http.MethodGet:
+// func usersHandler(w http.ResponseWriter, r *http.Request) {
+// 	//switch case that acats baased on either GET or POST
+// 	switch r.Method {
 
-		//Tells Postman to expect json type
-		w.Header().Set("Content-Type", "application/json")
-		//converts and sends users as json
-		json.NewEncoder(w).Encode(users)
+// 	case http.MethodGet:
 
-	case http.MethodPost:
+// 		//Tells Postman to expect json type
+// 		w.Header().Set("Content-Type", "application/json")
+// 		//converts and sends users as json
+// 		json.NewEncoder(w).Encode(users)
 
-		//creates a data struct to recieve the POST, the `...` is to tell GO that anything with name is to be stored here
-		var data struct {
-			Name string `json:"name"`
-		}
+// 	case http.MethodPost:
 
-		//r.Body is the raw request from POST aand this line will decode the message and store as a normal string, also checks if the message failed in translation
-		if err := json.NewDecoder(r.Body).Decode(&data); err != nil {
-			http.Error(w, "invalid request", http.StatusBadRequest)
-			return
-		}
+// 		//creates a data struct to recieve the POST, the `...` is to tell GO that anything with name is to be stored here
+// 		var data struct {
+// 			Name string `json:"name"`
+// 		}
 
-		//stores the POST'd data into my local 'users' slice
-		users = append(users, data.Name)
+// 		//r.Body is the raw request from POST aand this line will decode the message and store as a normal string, also checks if the message failed in translation
+// 		if err := json.NewDecoder(r.Body).Decode(&data); err != nil {
+// 			http.Error(w, "invalid request", http.StatusBadRequest)
+// 			return
+// 		}
 
-		//sends the 201 Creaated http status code
-		w.WriteHeader(http.StatusCreated)
-		w.Write([]byte("user added"))
+// 		//stores the POST'd data into my local 'users' slice
+// 		users = append(users, data.Name)
 
-	default:
-		http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
-	}
-}
+// 		//sends the 201 Creaated http status code
+// 		w.WriteHeader(http.StatusCreated)
+// 		w.Write([]byte("user added"))
 
-var users []string
+// 	default:
+// 		http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+// 	}
+// }
+
+// var users []string
 
 func main() {
-	http.HandleFunc("/users", usersHandler)
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
 
-	//starts the 8080 server
-	http.ListenAndServe(":5431", nil)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Successfully connected!")
 }
